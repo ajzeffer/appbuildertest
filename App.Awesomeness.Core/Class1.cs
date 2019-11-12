763afa76969fbc75b5d1e1a79538fdfd53d464e6
Adam Zeffer <azeffer@dtadmins-mbp.lan>
2019-11-12 04:51:35 -0700

    init: initial scaffolding

---
+++ App.Awesomeness.Core/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace App.Awesomeness.Core
+{
+    public class Class1
+    {
+    }
+}
---
+++ App.Awesomeness.Infrastructure/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace App.Awesomeness.Infrastructure
+{
+    public class Class1
+    {
+    }
+}
